06062a675fa2adfc6f33da815639dcc1b78d1351
louisamaya8 <louisamaya8@gmail.com>
2026-06-03 21:32:05 -0600

    Update scrpit.js

--- scrpit.js
+++ scrpit.js
@@ -1,1 +1,15 @@
 // Umbral AR
+
+const slides = document.querySelectorAll('.slide');
+
+let currentSlide = 0;
+
+function changeSlide(){
+    slides[currentSlide].classList.remove('active');
+
+    currentSlide = (currentSlide + 1) % slides.length;
+
+    slides[currentSlide].classList.add('active');
+}
+
+setInterval(changeSlide, 4000);
